14080dcb6f81d7d498a0a82a9c8128349f13b313
Misterxof <mishail.lukushevich@gmail.com>
2017-05-27 23:04:27 +0300

    5 commit

---
+++ FlightViewController.h
@@ -0,0 +1,15 @@
+//
+//  FlightViewController.h
+//  tp8.1
+//
+//  Created by Admin on 20.05.17.
+//  Copyright (c) 2017 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Record.h"
+#import "AppDelegate.h"
+
+@interface FlightViewController : UIViewController  <UITableViewDataSource, UITableViewDelegate>
+-(id)initWithCities:(NSString*)cityFrom cityTo: (NSString*)cityTo;
+@end
