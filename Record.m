36f48c63d2cc3e5ebe1b1ca4b99eba94f5d26efe
Misterxof <mishail.lukushevich@gmail.com>
2017-05-27 23:08:17 +0300

    9 commit

---
+++ Record.m
@@ -0,0 +1,19 @@
+//
+//  Record.m
+//  tp8.1
+//
+//  Created by Admin on 20.05.17.
+//  Copyright (c) 2017 Admin. All rights reserved.
+//
+
+#import "Record.h"
+
+
+@implementation Record
+
+@dynamic aviaCompany;
+@dynamic cityFrom;
+@dynamic cityTo;
+@dynamic price;
+
+@end
